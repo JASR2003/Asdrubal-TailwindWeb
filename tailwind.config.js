bdfed284c88dfc89d68928f9a7807e72f506dc3f
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2025-01-02 10:44:44 -0400

    act

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'blue-dark': '#020f1e',
+        'blue-back': '#041a2e',
+        'gray-txt': '#94a3b8',
+        'blue-txt': '#36d0f1',
+        'blue-hover': '#192a3b',
+        'blue-btn': '#032540',
+      },
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'], // Montserrat como fuente principal
+      },
+    },
+  },
+  plugins: [],
+}
