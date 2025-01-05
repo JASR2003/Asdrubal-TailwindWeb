4cdc475980537ee446a0d73bf045ea36d9791868
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2025-01-05 18:22:53 -0400

    cursor pointer changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        'custom-cursor': 'radial-gradient(circle, rgba(255,255,255,1) 30%, rgba(255,255,255,0.8) 40%, rgba(255,255,255,0.7) 50%, rgba(255,255,255,0.6) 60%, rgba(255,255,255,0.5) 70%, rgba(255,255,255,0.4) 80%, rgba(255,255,255,0.3) 90%, rgba(255,255,255,0.2) 95%, rgba(255,255,255,0))',
+        'custom-cursor': 'radial-gradient(rgba(179, 181, 212, 1), rgba(179, 181, 212, 0.5) 40%, transparent 70%);',
       },
     },
   },
