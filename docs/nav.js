c58b1be721d1561ed33f1cc80cf04a0863a6a2da
Jorge Suárez <jorgealejandrosuarez0807@gmail.com>
2025-01-11 19:52:28 -0400

    agg bg-white/30

--- docs/nav.js
+++ docs/nav.js
@@ -130,10 +130,12 @@ function adjustMobileMenuStyles() {
   if (isWebKit) {
     // Si es WebKit (iOS/Safari), elimina la clase backdrop-blur-sm y agrega bg-blue-hover
     mobileMenu.classList.remove('backdrop-blur-sm');
+    mobileMenu.classList.remove('bg-white/30');
     mobileMenu.classList.add('bg-blue-hover');
   } else {
     // Si no es WebKit (otros navegadores), asegúrate de restaurar las clases originales
     mobileMenu.classList.add('backdrop-blur-sm');
+    mobileMenu.classList.add('bg-white/30');
     mobileMenu.classList.remove('bg-blue-hover');
   }
 }
